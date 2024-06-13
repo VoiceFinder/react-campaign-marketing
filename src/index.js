88a417e3ebc648c8f83e2eeecbf111a47ab25849
최창규 <chang4293@gmail.com>
2024-06-13 15:28:34 +0900

    env

--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom/client';
 import './assets/styles/index.css';
 import App from './App';
 
+const loadKakaoMapScript = () => {
+  const script = document.createElement('script');
+  script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_API_KEY}&libraries=services`;
+  script.async = true;
+  document.head.appendChild(script);
+};
+
+const Root = () => {
+  useEffect(() => {
+    loadKakaoMapScript();
+  }, []);
+
+  return <App />;
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
 
