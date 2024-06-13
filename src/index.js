a34fd6fe83808341689afe21609d64dd6b84a7e5
최창규 <chang4293@gmail.com>
2024-06-13 19:07:44 +0900

    map

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 const loadKakaoMapScript = () => {
   const script = document.createElement('script');
-  script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_API_KEY}&libraries=services`;
+  script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_KAKAO_API_KEY}&libraries=services&autoload=false`;
   script.async = true;
   document.head.appendChild(script);
 };
